caff3df71fd54b5ed8a954a52179e1ecbb115901
Anilcan Kara <anilcankara.com@gmail.com>
2024-04-23 20:29:54 +0300

    Root path

--- api/root.js
+++ api/root.js
@@ -1,4 +1,4 @@
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 
-export const root = `${dirname(fileURLToPath(import.meta.url)+'/..')}`
+export const root = dirname(fileURLToPath(import.meta.url)).split('/').slice(0, -1).join('/')
