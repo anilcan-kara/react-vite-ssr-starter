2d9c1ac16965fc22ddc861310a8e23cd8ea20048
Anilcan Kara <anilcankara.com@gmail.com>
2024-04-23 20:27:13 +0300

    Root folder

--- api/root.js
+++ api/root.js
@@ -1,4 +1,4 @@
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 
-export const root = `${dirname(fileURLToPath(import.meta.url))}/..`
+export const root = `${dirname(fileURLToPath(import.meta.url)+'/..')}`
